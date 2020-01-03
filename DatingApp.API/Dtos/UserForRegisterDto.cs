b99586bb47b5fff8a3b4e60920b4959db3bafb51
Izzat Jamili <gih.nih@gmail.com>
2020-01-04 00:52:01 +0800

    End Section 3

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username {get; set;}
 
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "Please specify Password length between 4 and 8")]
         public string Password { get; set; }
     }
 }
